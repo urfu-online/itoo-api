bd4fef0d29a285fd169a2044425f647cf86a9f69
anonimD08 <asdebug@yandex.ru>
2019-02-12 16:33:32 +0500

    lol_literals

--- itoo_api/models.py
+++ itoo_api/models.py
@@ -4,13 +4,13 @@ Database ORM models managed by this Django app
 Please do not integrate directly with these models!!!  This app currently
 offers one programmatic API -- api.py for direct Python integration.
 """
+from __future__ import unicode_literals
 
 import re
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from model_utils.models import TimeStampedModel
-from __future__ import unicode_literals
 
 
 @python_2_unicode_compatible
