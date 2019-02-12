8f37215a1c2b5ef301b278e8a2d14e4c4be71df9
anonimD08 <asdebug@yandex.ru>
2019-02-12 16:00:04 +0500

    setup versions

--- setup.py
+++ setup.py
@@ -29,13 +29,7 @@ setup(
     ],
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'django>=1.11,<2.0',
-        'django-model-utils>=1.4.0',
-        'djangorestframework>=3.2.0,<3.7.0',
-        'djangorestframework-oauth>=1.1.0,<2.0.0',
-        'edx-django-oauth2-provider>=1.2.0',
-        'edx-drf-extensions>=2.0.0,<3.0.0',
-        'edx-opaque-keys>=0.1.2,<1.0.0',
+        'django>=1.8,<1.9',
         'Pillow',
     ],
 )
