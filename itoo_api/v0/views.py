b842756c6ca1bbbac5de52e098b43351640e9ffe
anonimD08 <asdebug@yandex.ru>
2019-02-12 16:18:25 +0500

    remove dich

--- itoo_api/v0/views.py
+++ itoo_api/v0/views.py
@@ -2,11 +2,7 @@
 """
 Views for itoo_api end points.
 """
-from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_oauth.authentication import OAuth2Authentication
 
 from itoo_api.models import Program
 from itoo_api.serializers import ProgramSerializer
@@ -19,5 +15,4 @@ class ProgramViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Program.objects.filter(active=True)  # pylint: disable=no-member
     serializer_class = ProgramSerializer
     lookup_field = 'short_name'
-    # authentication_classes = (OAuth2Authentication, JwtAuthentication, SessionAuthentication)
-    # permission_classes = (IsAuthenticated,)
+
