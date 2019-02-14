5bc8a37c2e6d389af142b5a3db3c07137186e66f
MasterGowen <mastergowen@gmail.com>
2019-02-14 11:34:57 +0500

    course#11

--- itoo_api/serializers.py
+++ itoo_api/serializers.py
@@ -2,49 +2,13 @@
 Data layer serialization operations.  Converts querysets to simple
 python containers (mainly arrays and dicts).
 """
-from course_api.serializers import _CourseApiMediaCollectionSerializer
 from courseware.courses import get_course_by_id
+from enrollment.serializers import CourseSerializer
 from opaque_keys.edx.keys import CourseKey
 from organizations.models import Organization
 from rest_framework import serializers
 
 from itoo_api.models import Program, ProgramCourse
-
-
-class CourseSerializer(serializers.Serializer):  # pylint: disable=abstract-method
-    """
-    Serializer for Course objects providing minimal data about the course.
-    Compare this with CourseDetailSerializer.
-    """
-
-
-    end = serializers.DateTimeField()
-    enrollment_start = serializers.DateTimeField()
-    enrollment_end = serializers.DateTimeField()
-    id = serializers.CharField()  # pylint: disable=invalid-name
-    media = _CourseApiMediaCollectionSerializer(source='*')
-    name = serializers.CharField(source='display_name_with_default_escaped')
-    number = serializers.CharField(source='display_number_with_default')
-    org = serializers.CharField(source='display_org_with_default')
-    short_description = serializers.CharField()
-    start = serializers.DateTimeField()
-    start_display = serializers.CharField()
-    start_type = serializers.CharField()
-    pacing = serializers.CharField()
-    mobile_available = serializers.BooleanField()
-    hidden = serializers.SerializerMethodField()
-    invitation_only = serializers.BooleanField()
-
-    # 'course_id' is a deprecated field, please use 'id' instead.
-    course_id = serializers.CharField(source='id', read_only=True)
-
-    def get_hidden(self, course_overview):
-        """
-        Get the representation for SerializerMethodField `hidden`
-        Represents whether course is hidden in LMS
-        """
-        catalog_visibility = course_overview.catalog_visibility
-        return catalog_visibility in ['about', 'none']
 
 
 # pylint: disable=too-few-public-methods
