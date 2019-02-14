56cc9c22df6577c2ab3731688fcdb4447807a109
anonimD08 <asdebug@yandex.ru>
2019-02-14 13:13:47 +0500

    test3

--- itoo_api/serializers.py
+++ itoo_api/serializers.py
@@ -3,12 +3,50 @@ Data layer serialization operations.  Converts querysets to simple
 python containers (mainly arrays and dicts).
 """
 from courseware.courses import get_course_by_id
-from enrollment.serializers import CourseSerializer
+from course_api.serializers import _CourseApiMediaCollectionSerializer
+from enrollment.serializers import CourseSerializer, ModeSerializer
+from course_modes.models import CourseMode
 from opaque_keys.edx.keys import CourseKey
 from organizations.models import Organization
 from rest_framework import serializers
 
 from itoo_api.models import Program, ProgramCourse
+
+
+class CourseSerializer(serializers.Serializer):  # pylint: disable=abstract-method
+    """
+    Serialize a course descriptor and related information.
+    """
+
+    course_id = serializers.CharField(source="id")
+    course_name = serializers.CharField(source="display_name_with_default")
+    name = serializers.CharField(source='display_name_with_default_escaped')
+    media = _CourseApiMediaCollectionSerializer(source='*')
+    enrollment_start = serializers.DateTimeField(format=None)
+    enrollment_end = serializers.DateTimeField(format=None)
+    course_start = serializers.DateTimeField(source="start", format=None)
+    course_end = serializers.DateTimeField(source="end", format=None)
+    invite_only = serializers.BooleanField(source="invitation_only")
+    start_display = serializers.CharField()
+    course_modes = serializers.SerializerMethodField()
+
+    def __init__(self, *args, **kwargs):
+        self.include_expired = kwargs.pop("include_expired", False)
+        super(CourseSerializer, self).__init__(*args, **kwargs)
+
+    def get_course_modes(self, obj):
+        """
+        Retrieve course modes associated with the course.
+        """
+        course_modes = CourseMode.modes_for_course(
+            obj.id,
+            include_expired=self.include_expired,
+            only_selectable=False
+        )
+        return [
+            ModeSerializer(mode).data
+            for mode in course_modes
+        ]
 
 
 # pylint: disable=too-few-public-methods
