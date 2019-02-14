2501532a43fb19aa19094ce7ec055e358537dc6f
anonimD08 <asdebug@yandex.ru>
2019-02-14 08:55:04 +0500

    fix url ooops

--- itoo_api/v0/urls.py
+++ itoo_api/v0/urls.py
@@ -4,11 +4,11 @@ URLS for itoo_api end points.
 # pylint: disable=invalid-name
 from rest_framework import routers
 
-from itoo_api.v0.views import ProgramViewSet, OrganizationViewSet, ProgramCourseSerializer
+from itoo_api.v0.views import ProgramViewSet, OrganizationViewSet, ProgramCourseViewSet
 
 router = routers.SimpleRouter()
 router.register(r'programs', ProgramViewSet)
-router.register(r'programs/courses', ProgramCourseSerializer)
+router.register(r'programs/courses', ProgramCourseViewSet)
 router.register(r'organizations', OrganizationViewSet)
 
 app_name = 'v0'
