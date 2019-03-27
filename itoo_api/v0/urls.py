89d3b87ff82542bf0dbcff27aa440ba62275a30b
anonimD08 <asdebug@yandex.ru>
2019-03-27 19:04:32 +0500

    url enroll

--- itoo_api/v0/urls.py
+++ itoo_api/v0/urls.py
@@ -3,6 +3,7 @@ URLS for itoo_api end points.
 """
 # pylint: disable=invalid-name
 from rest_framework import routers
+from django.conf import settings
 
 from itoo_api.v0.views import ProgramViewSet, OrganizationViewSet, ProgramCourseViewSet, EnrollmentViewSet
 
@@ -10,7 +11,7 @@ router = routers.SimpleRouter()
 router.register(r'programs', ProgramViewSet)
 router.register(r'link_courses', ProgramCourseViewSet)
 router.register(r'organizations', OrganizationViewSet)
-router.register(r'enrollment', EnrollmentViewSet, base_name='enrollment')
+router.register(r'enrollment/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN), EnrollmentViewSet, base_name='enrollment')
 
 app_name = 'v0'
 urlpatterns = router.urls
