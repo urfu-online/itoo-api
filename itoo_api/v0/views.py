8d6dcf0ad5cedb02c5d419b7104e11d32293d92a
anonimD08 <asdebug@yandex.ru>
2019-08-27 12:20:45 +0500

    PaidCoursesViewSet2#10

--- itoo_api/v0/views.py
+++ itoo_api/v0/views.py
@@ -169,7 +169,7 @@ class PaidCoursesCusViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = CourseOverview.objects.all() # pylint: disable=no-member
     serializer_class = CourseModeSerializer
-    lookup_field = 'course_id'
+    lookup_field = 'id'
 
     # def get(self, request, course_id=None):
     #     course_key = CourseKey.from_string(course_id)
