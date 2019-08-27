db12a1dbc33661ff6c3f0465b1fcf775d30c3da5
anonimD08 <asdebug@yandex.ru>
2019-08-27 13:22:59 +0500

    PaidCoursesViewSet2#15

--- itoo_api/serializers.py
+++ itoo_api/serializers.py
@@ -135,12 +135,12 @@ class CourseModeSerializer(serializers.ModelSerializer):
         model = CourseOverview
         fields = ('course_id','course_name','course_modes')
 
-    def get_courses(self, obj):
-        course_keys = [CourseKey.from_string(course.course_id) for course in obj.get_courses()]
-        # courses = [CourseOverview.get_from_id(course_key) for course_key in course_keys]
-        # qs = CourseMode.objects.filter(mode_slug="verified")
-        course_modes = [CourseMode.modes_for_course(course_key, only_selectable=False) for course_key in course_keys].filter(mode_slug="verified")
-        return ModeSerializer(course_modes, many=True).data
+    # def get_courses(self, obj):
+    #     course_keys = [CourseKey.from_string(course.course_id) for course in obj.get_courses()]
+    #     # courses = [CourseOverview.get_from_id(course_key) for course_key in course_keys]
+    #     # qs = CourseMode.objects.filter(mode_slug="verified")
+    #     course_modes = [CourseMode.modes_for_course(course_key, only_selectable=False) for course_key in course_keys]
+    #     return ModeSerializer(course_modes, many=True).data
 
     def get_course_modes(self, obj):
         course_modes = CourseMode.modes_for_course(
