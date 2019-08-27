c871de677acda2fa0e37efda26955ebd2bb763e5
MasterGowen <mastergowen@gmail.com>
2019-08-27 16:07:14 +0500

    unicode literals

--- itoo_api/models.py
+++ itoo_api/models.py
@@ -4,7 +4,7 @@ Database ORM models managed by this Django app
 Please do not integrate directly with these models!!!  This app currently
 offers one programmatic API -- api.py for direct Python integration.
 """
-from __future__ import unicode_literals
+
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -13,10 +13,10 @@ from model_utils.models import TimeStampedModel
 
 @python_2_unicode_compatible
 class Program(TimeStampedModel):
-    name = models.CharField(u'Название', blank=False, null=False, max_length=1024, default="")
-    short_name = models.CharField(u'Аббревиатура', blank=False, null=False, max_length=64, default="", unique=True)
-    slug = models.CharField(u'Человеко-понятный уникальный идентификатор', blank=False, null=False, max_length=64, default="", unique=True)
-    description = models.TextField(u'Описание')
+    name = models.CharField('Название', blank=False, null=False, max_length=1024, default="")
+    short_name = models.CharField('Аббревиатура', blank=False, null=False, max_length=64, default="", unique=True)
+    slug = models.CharField('Человеко-понятный уникальный идентификатор', blank=False, null=False, max_length=64, default="", unique=True)
+    description = models.TextField('Описание')
     logo = models.ImageField(
         upload_to='program_logos',
         help_text='Please add only .PNG files for logo images. This logo will be used on Program logo.',
@@ -37,13 +37,13 @@ class Program(TimeStampedModel):
 
     class Meta(object):
         """ Meta class for this Django model """
-        verbose_name = u'Программа'
-        verbose_name_plural = u'Программы'
+        verbose_name = 'Программа'
+        verbose_name_plural = 'Программы'
 
 
 @python_2_unicode_compatible
 class ProgramCourse(TimeStampedModel):
-    course_id = models.CharField(max_length=255, db_index=True, verbose_name=u'ID Курса')
+    course_id = models.CharField(max_length=255, db_index=True, verbose_name='ID Курса')
     program = models.ForeignKey(Program, db_index=True)
     active = models.BooleanField(default=True)
 
@@ -52,16 +52,16 @@ class ProgramCourse(TimeStampedModel):
 
     class Meta(object):
         """ Meta class for this Django model """
-        verbose_name = u'Ссылка на курс(Программа)'
-        verbose_name_plural = u'Ссылки на курс(Программы)'
+        verbose_name = 'Ссылка на курс(Программа)'
+        verbose_name_plural = 'Ссылки на курс(Программы)'
 
 
 @python_2_unicode_compatible
 class OrganizationCustom(TimeStampedModel):
-    name = models.CharField(u'Название', blank=False, null=False, max_length=1024, default="")
-    short_name = models.CharField(u'Аббревиатура', blank=False, null=False, max_length=64, default="", unique=True)
-    slug = models.CharField(u'Человеко-понятный уникальный идентификатор', blank=False, null=False, max_length=64, default="", unique=True)
-    description = models.TextField(u'Описание')
+    name = models.CharField('Название', blank=False, null=False, max_length=1024, default="")
+    short_name = models.CharField('Аббревиатура', blank=False, null=False, max_length=64, default="", unique=True)
+    slug = models.CharField('Человеко-понятный уникальный идентификатор', blank=False, null=False, max_length=64, default="", unique=True)
+    description = models.TextField('Описание')
     logo = models.ImageField(
         upload_to='org_logos',
         help_text='Please add only .PNG files for logo images. This logo will be used on Organization logo.',
@@ -82,13 +82,13 @@ class OrganizationCustom(TimeStampedModel):
 
     class Meta(object):
         """ Meta class for this Django model """
-        verbose_name = u'Организация'
-        verbose_name_plural = u'Организации'
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
 
 
 @python_2_unicode_compatible
 class OrganizationCourse(TimeStampedModel):
-    course_id = models.CharField(max_length=255, db_index=True, verbose_name=u'ID Курса')
+    course_id = models.CharField(max_length=255, db_index=True, verbose_name='ID Курса')
     org = models.ForeignKey(OrganizationCustom, db_index=True)
     active = models.BooleanField(default=True)
 
@@ -97,5 +97,5 @@ class OrganizationCourse(TimeStampedModel):
 
     class Meta(object):
         """ Meta class for this Django model """
-        verbose_name = u'Ссылка на курс(Организация)'
-        verbose_name_plural = u'Ссылки на курс(Организации)'
+        verbose_name = 'Ссылка на курс(Организация)'
+        verbose_name_plural = 'Ссылки на курс(Организации)'
