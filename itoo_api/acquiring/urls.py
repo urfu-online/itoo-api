2f25cd1e426945de4cd21c5bdf42005f3f778c84
anonimD08 <asdebug@yandex.ru>
2019-08-30 15:15:23 +0500

    url router + patterns: try #1

--- itoo_api/acquiring/urls.py
+++ itoo_api/acquiring/urls.py
@@ -3,12 +3,11 @@ from itoo_api.acquiring.views import *
 
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'paid_course_cus', PaidCoursesCusViewSet)
-app_name = 'acquiring'
+router_list = routers.SimpleRouter()
+router_list.register(r'paid_course_cus', PaidCoursesCusViewSet)
 
 urlpatterns = [
     url(r'paid_courses_role/', PaidCoursesRoleViewSet.as_view(), name='paid_courses_role'),
     url(r'course_mode_change/',CourseModesChange.as_view(), name='course_mode_change'),
-    url(r'^', include(router.urls)),
+    url(r'^list/', include(router_list.urls)),
 ]
