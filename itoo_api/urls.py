5335ccce02d5af748601532ae893578bf5324dec
anonimD08 <asdebug@yandex.ru>
2019-08-30 12:42:22 +0500

    Test course_mode change mode, try: #6

--- itoo_api/urls.py
+++ itoo_api/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^v0/', include('itoo_api.v0.urls')),
     url(r'^v0/', include('organizations.v0.urls')),
     url(r'^v0/paid_courses_role/', PaidCoursesRoleViewSet.as_view(), name='paid_courses_role'),
-    url(r'^v0/course_mode_change/{course_id_get}/', CourseModesChange.as_view(), name='course_mode_change')
+    url(r'^v0/course_mode_change/{course_id_get}/'.format(course_id_get=settings.COURSE_ID_PATTERN), CourseModesChange.as_view(), name='course_mode_change')
 #     .format(
     #         username=settings.USERNAME_PATTERN, course_key=settings.COURSE_ID_PATTERN)
 ]
