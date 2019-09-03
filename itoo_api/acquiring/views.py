1178fc1619da528071fb6c2a26c5891fa40dbad4
anonimD08 <asdebug@yandex.ru>
2019-09-03 18:42:08 +0500

    test pay_urfu endpoint #22

--- itoo_api/acquiring/views.py
+++ itoo_api/acquiring/views.py
@@ -191,6 +191,7 @@ class CourseModeListAllViewSet(viewsets.ReadOnlyModelViewSet):
 
 from django.core import serializers
 from django.utils import timezone
+from django.utils import six
 
 class PayUrfuDataViewSet(APIView):
     permission_classes = (AllowAny, )
@@ -201,6 +202,8 @@ class PayUrfuDataViewSet(APIView):
         return RESTResponse({"Success"})
 
     def get(self, request):
-        obj = PayUrfuData.objects.create(data=request.GET)
+        qd = dict(six.iterlists(request.GET))
+        logger.warning(qd)
+        obj = PayUrfuData.objects.create(data=qd)
         obj.save()
         return RESTResponse({"Success"})
