a9ed8ae12c391703e7b9295b9fee2ffbe8a318cf
anonimD08 <asdebug@yandex.ru>
2019-09-03 18:35:24 +0500

    test pay_urfu endpoint #20

--- itoo_api/acquiring/views.py
+++ itoo_api/acquiring/views.py
@@ -196,10 +196,10 @@ class PayUrfuDataViewSet(APIView):
     permission_classes = (AllowAny, )
 
     def post(self, request):
-        s_obj1 = json.loads(request.body)
+        s_obj1 = request.body
         # s_obj = serializers.serialize("json", [s_obj1])
         logger.warning(s_obj1)
-        obj2 = PayUrfuData.objects.create(data=json.dumps(s_obj1))
+        obj2 = PayUrfuData.objects.create(data=s_obj1)
         obj2.save()
         return RESTResponse({"Success"})
 
