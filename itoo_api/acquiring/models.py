2895052509928d73230102fca98a98d774722238
anonimD08 <asdebug@yandex.ru>
2019-09-03 16:22:58 +0500

    test pay_urfu endpoint #9

--- itoo_api/acquiring/models.py
+++ itoo_api/acquiring/models.py
@@ -46,8 +46,8 @@ class PayUrfuData(TimeStampedModel):
     data = models.TextField('Данные')
     pub_date = models.DateTimeField('Дата запроса')
 
-    def __str__(self):
-        return self.pub_date
+    def __unicode__(self):
+        return unicode(self.pub_date)
 
     class Meta(object):
         """ Meta class for this Django model """
