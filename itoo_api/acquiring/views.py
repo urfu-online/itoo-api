6775df60f7f26d1aab997a0880ceb5cbc0f0d390
anonimD08 <asdebug@yandex.ru>
2019-09-03 19:28:48 +0500

    test pay_urfu endpoint #29

--- itoo_api/acquiring/views.py
+++ itoo_api/acquiring/views.py
@@ -197,6 +197,7 @@ class PayUrfuDataViewSet(APIView):
     permission_classes = (AllowAny, )
 
     def post(self, request):
+        logger.warning(request)
         obj = PayUrfuData.objects.create(data=request.body)
         obj.save()
         return RESTResponse({"Success"})
