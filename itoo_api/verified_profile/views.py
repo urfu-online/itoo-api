4e1ce458fc220d7dc215c4e06dc0b50b2c352ad4
anonimD08 <asdebug@yandex.ru>
2019-09-12 09:46:59 +0500

    anketa#6

--- itoo_api/verified_profile/views.py
+++ itoo_api/verified_profile/views.py
@@ -5,6 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .forms import ProfileForm
 from django.shortcuts import render
+from django.template import Context, Template
+from xblock.fragment import Fragment
+import pkg_resources
 
 
 logging.basicConfig()
@@ -16,4 +19,33 @@ class VerifiedProfileView(APIView):
 
     def get(self, request):
         form = ProfileForm()
-        return render(request, 'templates/profile_edit.html', {'form': form})
+        fragment = Fragment()
+        fragment.add_content(
+            render_template(
+                'templates/profile_edit.html',
+                {'form': form}
+            )
+        )
+        return fragment
+        # return render(request, 'templates/profile_edit.html', {'form': form})
+
+
+def load_resource(resource_path):  # pragma: NO COVER
+    """
+    Gets the content of a resource
+    """
+    resource_content = pkg_resources.resource_string(__name__, resource_path)
+    return unicode(resource_content)
+
+
+def render_template(template_path, context=None):  # pragma: NO COVER
+    """
+    Evaluate a template by resource path, applying the provided context.
+    """
+    if context is None:
+        context = {}
+
+    template_str = load_resource(template_path)
+    template = Template(template_str)
+    return template.render(Context(context))
+
