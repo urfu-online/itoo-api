a1e991c2806ba3f0e7164ae8326b76328bdc6364
anonimD08 <asdebug@yandex.ru>
2019-09-12 11:11:53 +0500

    anketa#26

--- itoo_api/verified_profile/views.py
+++ itoo_api/verified_profile/views.py
@@ -23,27 +23,27 @@ def profile_new(request):
         'form': form
     }
     template_path = '../templates/profile_edit.html'
-    template_str = load_resource(template_path)
+    # template_str = load_resource(template_path)
     return render(request,template_path,context)
 
     # return render(request, 'templates/profile_edit.html', {'form': form})
 
 
-def load_resource(resource_path):  # pragma: NO COVER
-    """
-    Gets the content of a resource
-    """
-    resource_content = pkg_resources.resource_string(__name__, resource_path)
-    return unicode(resource_content)
-
-
-def render_template(template_path, context=None):  # pragma: NO COVER
-    """
-    Evaluate a template by resource path, applying the provided context.
-    """
-    if context is None:
-        context = {}
-
-    template_str = load_resource(template_path)
-    template = Template(template_str)
-    return template.render(Context(context))
+# def load_resource(resource_path):  # pragma: NO COVER
+#     """
+#     Gets the content of a resource
+#     """
+#     resource_content = pkg_resources.resource_string(__name__, resource_path)
+#     return unicode(resource_content)
+#
+#
+# def render_template(template_path, context=None):  # pragma: NO COVER
+#     """
+#     Evaluate a template by resource path, applying the provided context.
+#     """
+#     if context is None:
+#         context = {}
+#
+#     template_str = load_resource(template_path)
+#     template = Template(template_str)
+#     return template.render(Context(context))
