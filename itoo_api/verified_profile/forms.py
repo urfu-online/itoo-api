9ba0ec07a3ec21617852f5895040492b0daaba97
anonimD08 <asdebug@yandex.ru>
2019-09-12 12:07:26 +0500

    anketa#33

--- itoo_api/verified_profile/forms.py
+++ itoo_api/verified_profile/forms.py
@@ -1,14 +1,19 @@
 from django import forms
 from .models import Profile
 
-class ProfileForm(forms.ModelForm):
 
+class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
         exclude = ['all_valid']
         widgets = {
             'first_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'last_name': forms.TextInput(
                 attrs={
                     'class': 'form-control'
                 }
