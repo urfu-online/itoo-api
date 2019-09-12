8e0a37274d6641f6c019a41864b889957f1d8115
anonimD08 <asdebug@yandex.ru>
2019-09-12 08:59:24 +0500

    anketa begin

--- itoo_api/__init__.py
+++ itoo_api/__init__.py
@@ -2,6 +2,6 @@
 itoo-api app initialization module
 """
 
-# from verified_profile.models import Offer, Profile
+from verified_profile.models import Offer, Profile
 
 __version__ = '0.0.1'  # pragma: no cover
