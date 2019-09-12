29a3c27a9da477c4b632832e54cc27e0840ed069
anonimD08 <asdebug@yandex.ru>
2019-09-12 11:48:00 +0500

    anketa#29

--- itoo_api/verified_profile/forms.py
+++ itoo_api/verified_profile/forms.py
@@ -6,3 +6,30 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'second_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'city': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'birth_date': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'phone': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }
