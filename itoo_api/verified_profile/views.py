abd120a5af5aa883fd556d63575253a3402a451a
anonimD08 <asdebug@yandex.ru>
2019-09-17 13:15:57 +0500

    test pay urfu #5

--- itoo_api/verified_profile/views.py
+++ itoo_api/verified_profile/views.py
@@ -35,6 +35,7 @@ def profile_new(request):
             profile_params = {
                 'profile': profile
             }
+            logger.debug(profile_params)
             my_render =  redirect_params('https://ubu.urfu.ru/pay/', profile_params)
         else:
             my_render = render(request, template_path, {'form': form })
