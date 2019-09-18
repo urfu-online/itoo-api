223b45f8bb5821babae6f5e0d871a263ed04c3ab
anonimD08 <asdebug@yandex.ru>
2019-09-18 10:59:06 +0500

    test pay urfu #19

--- itoo_api/utils.py
+++ itoo_api/utils.py
@@ -6,7 +6,7 @@ import os
 
 def generate_new_filename(instance, filename):
     f, ext = os.path.splitext(filename)
-    filename = '%s%s%s' % (uuid.uuid4().hex, ext, instance.user.user.username)
+    filename = '%s%s%s' % (uuid.uuid4().hex, ext, instance.first_name)
     fullpath = "verified_profile/{subdir}/{filename}".format(
         subdir=time.strftime('%Y-%m'),
         filename=filename
