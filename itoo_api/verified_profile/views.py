2f5f94f4431bc56a654abd2a209bccb5dc6e9dcb
anonimD08 <asdebug@yandex.ru>
2019-10-02 15:26:33 +0500

    templates detail#26

--- itoo_api/verified_profile/views.py
+++ itoo_api/verified_profile/views.py
@@ -30,7 +30,7 @@ def redirect_params(url, params=None):
 
 def profile_new(request):
     if request.method == "POST":
-        form = ProfileForm(request.POST, request.FILES, instance=request.user)
+        form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
             profile = form.save(commit=False)
             profile.user = request.user
