335c4a66c03c06b492a2941781656e43267d4c22
anonimD08 <asdebug@yandex.ru>
2019-11-14 13:52:05 +0500

    EnrollProgram #47

--- itoo_api/v0/views.py
+++ itoo_api/v0/views.py
@@ -47,7 +47,9 @@ class MultipleFieldLookupMixin(object):
         queryset = self.get_queryset()  # Get the base queryset
         queryset = self.filter_queryset(queryset)  # Apply any filter backends
         filter = {}
+        logger.warning(str(self.lookup_fields))
         for field in self.lookup_fields:
+            logger.warning(str(field))
             if self.kwargs[field]:  # Ignore empty fields.
                 filter[field] = self.kwargs[field]
         obj = get_object_or_404(queryset, **filter)  # Lookup the object
