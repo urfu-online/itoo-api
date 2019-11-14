0a5a038552cbe87c1f96e5b37b7e6646b2f4111e
anonimD08 <asdebug@yandex.ru>
2019-11-14 16:37:44 +0500

    EnrollProgram #71

--- itoo_api/v0/views.py
+++ itoo_api/v0/views.py
@@ -71,7 +71,7 @@ class EnrollProgramViewSet(viewsets.ModelViewSet):
                     if enroll_program:
                         return Response(EnrollProgramSerializer(enroll_program).data)
                     else:
-                        return Response()
+                        return Response('')
                 except ObjectDoesNotExist:
                     return None
 
