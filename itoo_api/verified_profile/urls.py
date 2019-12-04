5040510e59751aab9b7f7e83c49686b5277cda04
anonimD08 <asdebug@yandex.ru>
2019-12-04 15:32:42 +0500

    anketa edit template #6

--- itoo_api/verified_profile/urls.py
+++ itoo_api/verified_profile/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'profile/$', profile_detail, name='profile_detail'),
     url(r'profile/new/$', profile_new, name='profile_new'),
     url(r'profile/edit/$', profile_edit, name='profile_edit'),
-    url(r'profile/edit_exist/$', profile_edit, name='profile_edit_exist')
+    url(r'profile/edit_exist/$', profile_edit_exist, name='profile_edit_exist')
     # url(r'paid_course_cus/', PaidCoursesCusViewSet, name='paid_course_cus')
     # url(r'^list/', include(router_list.urls)),
 ]
