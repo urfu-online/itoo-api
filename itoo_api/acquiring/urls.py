da8dda507712fea3d5310e19f237f495ebc021b7
anonimD08 <asdebug@yandex.ru>
2019-12-06 16:22:28 +0500

    checkSession #4

--- itoo_api/acquiring/urls.py
+++ itoo_api/acquiring/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from itoo_api.acquiring.views import *
-
+from itoo_api.v0.views import CheckSessionID
 from rest_framework import routers
 
 router_list = routers.DefaultRouter()
@@ -9,8 +9,9 @@ router_list.register(r'courses_mode_all', CourseModeListAllViewSet)
 app_name = 'acquiring'
 urlpatterns = [
     url(r'user_mode_change/', ChangeModeStateUserViewSet.as_view(), name='paid_courses_role'),
-    url(r'course_mode_change/',CourseModesChange.as_view(), name='course_mode_change'),
+    url(r'course_mode_change/', CourseModesChange.as_view(), name='course_mode_change'),
     url(r'pay_urfu/', PayUrfuDataViewSet.as_view(), name='pay_urfu'),
+    url(r'check_session/', CheckSessionID.as_view(), name='check_session'),
     # url(r'paid_course_cus/', PaidCoursesCusViewSet, name='paid_course_cus')
     url(r'^list/', include(router_list.urls)),
 ]
