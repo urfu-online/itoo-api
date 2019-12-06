f42cfc5385eb6684f9e44a15417b0843e6e0402d
anonimD08 <asdebug@yandex.ru>
2019-12-06 16:44:35 +0500

    checkSession #6

--- itoo_api/v0/views.py
+++ itoo_api/v0/views.py
@@ -33,8 +33,10 @@ logger = logging.getLogger(__name__)
 class CheckSessionID(APIView):
 
     def post(self, request):
-        my_key = request.session.get('sessionid', None)
-        logger.warn(my_key)
+        my_key = request.COOKIES.get('sessionid', None)
+        logger.warn(request.COOKIES)
+        logger.warn(request.session)
+        logger.warn(request)
         engine = import_module(settings.SESSION_ENGINE)
         session = engine.SessionStore(my_key)
 
