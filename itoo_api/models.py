107b56deb144625e16431f837f7e2aa5c04f076a
MasterGowen <mastergowen@gmail.com>
2020-02-05 14:17:22 +0500

    Remove dummy Offer model#1

--- itoo_api/models.py
+++ itoo_api/models.py
@@ -5,14 +5,12 @@ Please do not integrate directly with these models!!!  This app currently
 offers one programmatic API -- api.py for direct Python integration.
 """
 
-from django.db import models
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
 from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from model_utils.models import TimeStampedModel
-from django.contrib.auth.models import User
-
-from verified_profile.models import Offer, Profile
 
 
 @python_2_unicode_compatible
