20084fd862544f9f06035fd1bf69c7f5f068c51e
MasterGowen <mastergowen@gmail.com>
2020-02-08 14:12:25 +0500

    PMTS#10

--- itoo_api/acquiring/permissions.py
+++ itoo_api/acquiring/permissions.py
@@ -1,10 +1,11 @@
 from rest_framework import permissions
 
 
-class OwnerPermission(permissions.BasePermission):
-    """
-    Object-level permission to only allow updating his own profile
-    """
+class OwnerPermission(BasePermission):
+    message = 'You must be the owner of this object.'
 
-    def has_permission(self, request, view, obj):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
         return obj.user == request.user
