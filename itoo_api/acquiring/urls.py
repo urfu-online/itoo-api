b1bb972ddf52603110c41b60cd23536ce5316666
anonimD08 <asdebug@yandex.ru>
2020-02-08 12:44:26 +0500

    create_payment #2

--- itoo_api/acquiring/urls.py
+++ itoo_api/acquiring/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 
 router_list = routers.DefaultRouter()
 router_list.register(r'courses_mode_all', CourseModeListAllViewSet)
-router_list.register(r'payment', CreatePaymentViewSet)
+router_list.register(r'create_payment', CreatePaymentViewSet, base_name="create_payment")
 
 app_name = 'acquiring'
 urlpatterns = [
