19b17b257157e7579479aa4a9276112d0406ea45
Anastasiya <ovchinnickova.anast@gmail.com>
2020-02-12 18:13:36 +0500

    model change for payment #8

--- itoo_api/acquiring/views.py
+++ itoo_api/acquiring/views.py
@@ -360,7 +360,11 @@ class PaymentViewSet(viewsets.ModelViewSet):
             logger.warning('''Response payment: {}'''.format(payment_request))
             response_dicts = json.loads(payment_request.text)
             contract_number = None
+            logger.warning("!!!!!!!!!!!!!")
+            logger.warning(response_dicts)
             for response_dict in response_dicts:
+                logger.warning(response_dict)
+                logger.warning(response_dict[0])
                 if response_dict['result']:
                     contract_number = response_dict['result']['НомерДоговора']
                     payment.status = "1"
