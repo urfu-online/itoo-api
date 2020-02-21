3172262227d6c8b31994bd33d475d5761ece9f8b
anonimD08 <asdebug@yandex.ru>
2020-02-21 19:23:41 +0500

    reflection model|admin #1

--- itoo_api/reflection/models.py
+++ itoo_api/reflection/models.py
@@ -10,35 +10,6 @@ from itoo_api.models import Program
 
 
 @python_2_unicode_compatible
-class Question(TimeStampedModel):
-    title = models.CharField("Текст вопроса", max_length=256, blank=False, null=True)
-    order = models.PositiveSmallIntegerField("Порядок отрисовки", blank=True, null=True)
-    reflection = models.ForeignKey("Reflection", null=True, blank=True, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = "Вопрос"
-        verbose_name_plural = "Вопросы"
-
-    def __str__(self):
-        return str(self.title)
-
-
-@python_2_unicode_compatible
-class Answer(TimeStampedModel):
-    user = models.ForeignKey("User", null=True, blank=True, on_delete=models.CASCADE)
-    question = models.ForeignKey("Question", null=True, blank=True, on_delete=models.CASCADE)
-    answer_text = models.TextField("Ответ в формате текст", blank=True, null=True)
-    answer_float = models.FloatField("Ответ в формате числа", blank=True, null=True)
-
-    class Meta:
-        verbose_name = "Ответ"
-        verbose_name_plural = "Ответы"
-
-    def __str__(self):
-        return str(self.user.email)
-
-
-@python_2_unicode_compatible
 class Reflection(TimeStampedModel):
     title = models.CharField("Наименование", max_length=255, null=True, blank=False)
     description = models.TextField("Описание", null=True, blank=True)
@@ -53,6 +24,35 @@ class Reflection(TimeStampedModel):
 
     def __str__(self):
         return self.title
+
+
+@python_2_unicode_compatible
+class Question(TimeStampedModel):
+    title = models.CharField("Текст вопроса", max_length=256, blank=False, null=True)
+    order = models.PositiveSmallIntegerField("Порядок отрисовки", blank=True, null=True)
+    reflection = models.ForeignKey(Reflection, null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+    def __str__(self):
+        return str(self.title)
+
+
+@python_2_unicode_compatible
+class Answer(TimeStampedModel):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer_text = models.TextField("Ответ в формате текст", blank=True, null=True)
+    answer_float = models.FloatField("Ответ в формате числа", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+
+    def __str__(self):
+        return str(self.user.email)
 
 # @python_2_unicode_compatible
 # class ResultAnswer(TimeStampedModel):
