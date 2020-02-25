310cfc1bf766cc1716d913760dbc96e84c7982dc
anonimD08 <asdebug@yandex.ru>
2020-02-25 13:21:08 +0500

    reflection html #2

--- itoo_api/urls.py
+++ itoo_api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^v0/', include('itoo_api.v0.urls')),
     url(r'^acquiring/', include('itoo_api.acquiring.urls')),
     url(r'^verified_profile/', include('itoo_api.verified_profile.urls', namespace='verified_profile')),
-    url('r^reflection/', include('itoo_api.reflection.urls', namespace='reflection')),
+    url(r'^reflection/', include('itoo_api.reflection.urls', namespace='reflection')),
     url(r'^v0/', include('organizations.v0.urls')),
     # url(r'^v0/course_mode_change/(?P<course_id_get>[^/.]+)/', CourseModesChange.as_view(), name='course_mode_change')
 #     .format(
