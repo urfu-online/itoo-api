a78008e04d28f979f8aa744c7d3b1a27bc0f0753
vesloguzov <vesloguzov@gmail.com>
2020-02-25 18:30:54 +0500

    edit_exist #2

--- itoo_api/verified_profile/views.py
+++ itoo_api/verified_profile/views.py
@@ -298,8 +298,9 @@ def profile_edit_exist(request):
     # launch = dict()
     user = request.user
 
+
     logger.warning("!!!!!!!!!!!!!!!!!")
-    logger.warning(user)
+    logger.warning(user.is_anonymous)
 
     profile = Profile.get_profile(user=user)[0]
 
