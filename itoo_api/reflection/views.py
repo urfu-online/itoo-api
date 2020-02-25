260557b00912bdaffd8ff5b1afe721f481cbf789
anonimD08 <asdebug@yandex.ru>
2020-02-25 16:43:45 +0500

    reflection test #1

--- itoo_api/reflection/views.py
+++ itoo_api/reflection/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView
-from itoo_api.reflection.models import Reflection, Question
+from itoo_api.reflection.models import Reflection, Question, Answer
+from django import forms
+from django.views.generic.edit import FormMixin
 
 
 class ReflectionDetail(DetailView):
@@ -9,4 +11,32 @@ class ReflectionDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ReflectionDetail, self).get_context_data(**kwargs)
         context['questions'] = Question.objects.filter(reflection=self.get_object())
+        return context
+
+
+class AnswerForm(forms.Form):
+    widgets = {
+        'answer_text': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'required': 'true'
+            }
+        ),
+        'answer_float': forms.FloatField(
+            attrs={
+                'class': 'form-control',
+                'required': 'true'
+            }
+        ),
+    }
+
+
+class AnswerDetail(DetailView):
+    model = Answer
+
+    def get_context_data(self, **kwargs):
+        context = super(AnswerDetail, self).get_context_data(**kwargs)
+        context['form'] = AnswerForm()
+        context['questions'] = Answer.objects.filter(question=self.get_object())
+        context['reflections'] = Answer.objects.filter(question=Question.objects.filter(reflection=self.get_object())[0])
         return context
