9c239cc35f2c28ce36e698f5b08e118176c2005d
anonimD08 <asdebug@yandex.ru>
2020-02-25 14:07:10 +0500

    reflection html #11

--- itoo_api/reflection/views.py
+++ itoo_api/reflection/views.py
@@ -4,6 +4,7 @@ from itoo_api.reflection.models import Reflection, Question
 
 class ReflectionDetail(DetailView):
     model = Reflection
+    template_name = 'itoo_api/templates/IPMG/reflection_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super(ReflectionDetail, self).get_context_data(**kwargs)
