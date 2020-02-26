5cc114a5943a01724421fd04374eb0ff06e5f066
anonimD08 <asdebug@yandex.ru>
2020-02-26 17:41:49 +0500

    slug change #15

--- itoo_api/v0/views.py
+++ itoo_api/v0/views.py
@@ -133,7 +133,7 @@ class ProgramViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Program.objects.filter(active=True)  # pylint: disable=no-member
     serializer_class = ProgramSerializer
-    lookup_field = 'project_slug'
+    lookup_field = 'slug'
 
 
 class ProgramCourseViewSet(viewsets.ReadOnlyModelViewSet):
