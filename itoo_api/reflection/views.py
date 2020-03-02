ce2076ebf88e499d8f447da44d68624f2d3519cb
anonimD08 <asdebug@yandex.ru>
2020-03-02 18:10:29 +0500

    reflection view #28

--- itoo_api/reflection/views.py
+++ itoo_api/reflection/views.py
@@ -69,11 +69,10 @@ class ReflectionDetail(CreateView):
         self.object = None
         form_class = self.get_form_class()
         # form = self.get_form(form_class)
-        question_form = QuestionFormSet()
+        # question_form = QuestionFormSet()
         answer_form = AnswerFormSet()
         return self.render_to_response(
             self.get_context_data(form=self.get_object(),
-                                  question_form=question_form,
                                   answer_form=answer_form))
 
     def post(self, request, *args, **kwargs):
@@ -85,7 +84,6 @@ class ReflectionDetail(CreateView):
         self.object = None
         form_class = self.get_form_class()
         # form = self.get_form(form_class)
-        question_form = QuestionFormSet(self.request.POST)
         answer_form = AnswerFormSet(self.request.POST)
         if (answer_form.is_valid()):
             return self.form_valid(answer_form)
@@ -110,46 +108,45 @@ class ReflectionDetail(CreateView):
         return self.render_to_response(
             self.get_context_data(answer_form=answer_form))
 
-
-class AnswerDetail(DetailView, FormMixin):
-    model = Answer
-    template_name = '../templates/IPMG/reflection_detail.html'
-
-    def get_success_url(self):
-        from django.contrib import messages
-        messages.add_message(self.request, messages.INFO, 'Ваш ответ успешно записан')
-        return reverse('itoo:reflection:answer_detail', kwargs={'pk': self.object.pk})
-
-    def get_context_data(self, **kwargs):
-        context = super(AnswerDetail, self).get_context_data(**kwargs)
-        context['form'] = AnswerForm()
-        context['questions'] = Answer.objects.filter(question=self.get_object())
-        context['reflections'] = Answer.objects.filter(
-            question=Question.objects.filter(reflection=self.get_object())[0])
-        return context
-
-    def post(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return HttpResponseForbidden()
-        self.object = self.get_object()
-        form = self.get_form()
-        logger.warning(form)
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        # Here, we would record the user's interest using the message
-        # passed in form.cleaned_data['message']
-        question = Question.objects.filter(reflection=self.get_object())
-        logger.warning('!!!!!!!!!!!!!!!!')
-        logger.warning(form.cleaned_data['answer_text'])
-        logger.warning(question)
-        logger.warning(type(question))
-        for obj in question:
-            for each in form.cleaned_data['answer_text']:
-                # logger.warning('****', each, '****', type(each))
-
-                Answer.objects.create(user=self.request.user, question=obj, answer_text=each)
-        return super(AnswerDetail, self).form_valid(form)
+# class AnswerDetail(DetailView, FormMixin):
+#     model = Answer
+#     template_name = '../templates/IPMG/reflection_detail.html'
+#
+#     def get_success_url(self):
+#         from django.contrib import messages
+#         messages.add_message(self.request, messages.INFO, 'Ваш ответ успешно записан')
+#         return reverse('itoo:reflection:answer_detail', kwargs={'pk': self.object.pk})
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(AnswerDetail, self).get_context_data(**kwargs)
+#         context['form'] = AnswerForm()
+#         context['questions'] = Answer.objects.filter(question=self.get_object())
+#         context['reflections'] = Answer.objects.filter(
+#             question=Question.objects.filter(reflection=self.get_object())[0])
+#         return context
+#
+#     def post(self, request, *args, **kwargs):
+#         if not request.user.is_authenticated:
+#             return HttpResponseForbidden()
+#         self.object = self.get_object()
+#         form = self.get_form()
+#         logger.warning(form)
+#         if form.is_valid():
+#             return self.form_valid(form)
+#         else:
+#             return self.form_invalid(form)
+#
+#     def form_valid(self, form):
+#         # Here, we would record the user's interest using the message
+#         # passed in form.cleaned_data['message']
+#         question = Question.objects.filter(reflection=self.get_object())
+#         logger.warning('!!!!!!!!!!!!!!!!')
+#         logger.warning(form.cleaned_data['answer_text'])
+#         logger.warning(question)
+#         logger.warning(type(question))
+#         for obj in question:
+#             for each in form.cleaned_data['answer_text']:
+#                 # logger.warning('****', each, '****', type(each))
+#
+#                 Answer.objects.create(user=self.request.user, question=obj, answer_text=each)
+#         return super(AnswerDetail, self).form_valid(form)
