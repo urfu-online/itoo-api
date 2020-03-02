bc36d51e992fbeeb6d1b1f181fbe9a7efa9574a0
anonimD08 <asdebug@yandex.ru>
2020-03-02 16:49:29 +0500

    reflection view #16

--- itoo_api/reflection/views.py
+++ itoo_api/reflection/views.py
@@ -76,8 +76,14 @@ class ReflectionDetail(DetailView, FormMixin):
         return super(ReflectionDetail, self).form_valid(form)
 
 
-class AnswerDetail(DetailView):
+class AnswerDetail(DetailView, FormMixin):
     model = Answer
+    template_name = '../templates/IPMG/reflection_detail.html'
+
+    def get_success_url(self):
+        from django.contrib import messages
+        messages.add_message(self.request, messages.INFO, 'Ваш ответ успешно записан')
+        return reverse('itoo:reflection:reflection_detail', kwargs={'pk': self.object.pk})
 
     def get_context_data(self, **kwargs):
         context = super(AnswerDetail, self).get_context_data(**kwargs)
@@ -86,3 +92,29 @@ class AnswerDetail(DetailView):
         context['reflections'] = Answer.objects.filter(
             question=Question.objects.filter(reflection=self.get_object())[0])
         return context
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.get_form()
+        logger.warning(form)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        # Here, we would record the user's interest using the message
+        # passed in form.cleaned_data['message']
+        question = Question.objects.filter(reflection=self.get_object())
+        logger.warning('!!!!!!!!!!!!!!!!')
+        logger.warning(form.cleaned_data['answer_text'])
+        logger.warning(question)
+        logger.warning(type(question))
+        for obj in question:
+            for each in form.cleaned_data['answer_text']:
+                # logger.warning('****', each, '****', type(each))
+
+                Answer.objects.create(user=self.request.user, question=obj, answer_text=each)
+        return super(AnswerDetail, self).form_valid(form)
