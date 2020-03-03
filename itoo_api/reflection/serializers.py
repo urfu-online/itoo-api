c4543349251fc4f83e988ad570dc67477bdd73d0
anonimD08 <asdebug@yandex.ru>
2020-03-03 19:46:54 +0500

    reflection serializer #5

--- itoo_api/reflection/serializers.py
+++ itoo_api/reflection/serializers.py
@@ -39,10 +39,16 @@ class AnswerSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         answer_data = validated_data.pop('answer')
-        if User.objects.get(username=answer_data['username']) and Reflection.objects.get(
-                id=answer_data['reflection']) and Question.objects.get(id=answer_data['question']):
+        try:
+            for obj in answer_data['question']:
+                logger.warning(obj)
+            User.objects.get(username=answer_data['username']) and Reflection.objects.get(
+                id=answer_data['reflection']) and Question.objects.get(id=answer_data['question'])
             user = User.objects.get(username=answer_data['username'])
-            answer = Answer.objects.create(user=user, **answer_data)
-            return answer
-        else:
+            if User.objects.get(username=answer_data['username']) and Reflection.objects.get(
+                id=answer_data['reflection']) and Question.objects.get(id=answer_data['question']):
+                answer = Answer.objects.create(user=user, **answer_data)
+                return answer
+            else: Response({'detail': 'failed'})
+        except:
             return Response({'detail': 'failed'})
