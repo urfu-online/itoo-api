ae4f9094670ed94efd2020031022f467e65dac7e
anonimD08 <asdebug@yandex.ru>
2020-03-03 20:45:30 +0500

    reflection serializer #5

--- itoo_api/reflection/serializers.py
+++ itoo_api/reflection/serializers.py
@@ -29,9 +29,9 @@ class QuestionSerializer(serializers.ModelSerializer):
 
 
 class AnswerSerializer(serializers.ModelSerializer):
-    question = QuestionSerializer(many=False, required=True)
+    question = QuestionSerializer(many=False, required=True, read_only=True)
     # reflection = ReflectionSerializer(many=False, required=False)
-    username = serializers.CharField(source="user.username")
+    username = serializers.CharField(source="user.username", read_only=True)
 
     class Meta:
         model = Answer
