5d96b1a32c76daa30325418a40095cce21a108ee
MasterGowen <mastergowen@gmail.com>
2020-03-03 20:25:01 +0500

    profile_redirect#7

--- itoo_api/verified_profile/views.py
+++ itoo_api/verified_profile/views.py
@@ -33,7 +33,7 @@ def profile_redirect(request):
     :return:
     """
     program_slug = request.GET.get('program_slug', None)
-    print(request.META, request.path, request.path_unfo, "!!!!!!!!!!!!!!!!!!!!!")
+    print(request.META, request.path, request.path_info, "!!!!!!!!!!!!!!!!!!!!!")
     if program_slug:
         print(program_slug, '!!!!!!!!!!!!!!!!!')
         return redirect(reverse('itoo:verified_profile:profile_detail', kwargs={"slug": program_slug}))
