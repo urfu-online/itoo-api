5947a7c1d9c6f617b4c53bfb39c743421e0441e5
anonimD08 <asdebug@yandex.ru>
2020-03-05 11:50:20 +0500

    reflection #7

--- itoo_api/reflection/views.py
+++ itoo_api/reflection/views.py
@@ -211,8 +211,10 @@ class AnswerViewSet(viewsets.ModelViewSet):
         else:
             serializer = self.get_serializer(data=request.data, many=True)
             serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
+            logger.warning(serializer)
+            serializer.save()
             logger.warning(serializer.data)
+            # self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
