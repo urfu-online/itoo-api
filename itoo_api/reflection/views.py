385a6460fada63084d1e9712624bf9e4a47db476
anonimD08 <asdebug@yandex.ru>
2020-03-06 13:16:40 +0500

    reflection request.user #5

--- itoo_api/reflection/views.py
+++ itoo_api/reflection/views.py
@@ -190,14 +190,14 @@ class AnswerViewSet(viewsets.ModelViewSet):
             try:
                 for item in request.data:
                     question = get_object_or_404(Question, id=item.get('question'))
-                    user = get_object_or_404(User, user=request.user)
-                    logger.warning(user)
+                    # user = get_object_or_404(User, user=request.user)
+                    # logger.warning(user)
                     logger.warning('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                     reflection = get_object_or_404(Reflection, id=item.get('reflection'))
                     serializer = self.get_serializer(data=item, many=isinstance(item, list))
                     logger.warning(serializer)
                     serializer.is_valid(raise_exception=True)
-                    serializer.save(question=question, user=user, reflection=reflection)
+                    serializer.save(question=question, user=request.user, reflection=reflection)
 
                 return Response({'detail': 'success'}, status=status.HTTP_201_CREATED)
             except:
